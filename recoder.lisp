fcacf9f544be7a353498f6a9a30e8c42ff30b126
Nick Matvyeyev <mnasoft@gmail.com>
2015-05-10 17:55:06 +0300

    Написана функция разбора заголовка тренда

--- recoder.lisp
+++ recoder.lisp
@@ -25,111 +25,14 @@
       (push (read-byte in) lst))
     (reverse lst)))
 
-(defun read-trend-header (path &optional (byte-number 30))
-  (let ((lst nil)
-	(in (open path :element-type 'unsigned-byte)))
-    (dotimes (i byte-number)
-      (push (read-byte in) lst))
-    (close in)
-    (reverse lst)))
-
-(let (
-      (name-wid 10)
-      (opis-wid 40)
-      (dim-wid  24)
-      )
-  (defun analog-read(buffer num-rec)
-      (list (recode-string buffer (+ start
-			      (* num-rec
-				 (+ name-wid opis-wid dim-wid))) name-wid)
-	(recode-string buffer (+ start name-wid
-			      (* num-rec (+ name-wid opis-wid dim-wid))) opis-wid)
-	(recode-string buffer (+ start name-wid opis-wid
-			      (* num-rec (+ name-wid opis-wid dim-wid))) dim-wid))
-    ))
-
-(let (
-      (name-wid 10)
-      (opis-wid 40)
-      )
-(defun diskret-read(buffer number)
-    ))
+(defun list-to-int(list-of-int &optional (len (length list-of-int )))
+  "Выполняет преобразование списка целых чисел 
+находящихся в диапазоне 0 - 255 в целое число"
+  (let ((rez 0))
+    (dotimes (i len rez)
+      (setf (ldb (byte 8  (* 8 i)) rez) (nth i list-of-int)))))
 
 
-;(let ((start 30)
-;      (name-wid 10)
-;      (opis-wid 40)
-;      (dim-wid  24)
-;      (num-rec  387)
-;      )
-;  (list (recode-string bbb (+ start
-;			      (* num-rec
-;				 (+ name-wid opis-wid dim-wid))) name-wid)
-;	(recode-string bbb (+ start name-wid
-;			      (* num-rec (+ name-wid opis-wid dim-wid))) opis-wid)
-;	(recode-string bbb (+ start name-wid opis-wid
-;			      (* num-rec (+ name-wid opis-wid dim-wid))) dim-wid)))
-
-;(defparameter bbb (read-trend-header #p"D:/home/_namatv/git/11.trd" (+ 64676 30)))
-; (read-trend-hdr #p"D:/home/_namatv/git/11.trd")
-
-
-
-;(defparameter bbb(read-trd-file in 5))
-
-;(read-byte in)
-					;(recode-string bbb)
-
-(defun read-trend-hdr (in &optional (byte-number 30))
-  (let ((bufer nil)   ;; Буфер для чтения данных
-	(analog nil)  ;; Аналоговые сигналы
-	(discret nil) ;; Дискретные сигналы
-	
-	(head-wid 30)
-	(head-id-wid 5)	     ;; Строка идентификации
-	(head-version-wid 1) ;; Версия данных тренда
-	(head-date-wid 3)    ;; День Месяц Год-2000
-	(head-time-wid 3)    ;; Час Минута Секунда
-
-	(id nil)	  ;; Строка идентификации
-	(version nil)	  ;; Версия данных трендера
-	(date-day nil)	  ;; День
-	(date-month nil)  ;; Месяц 
-	(date-year nil)	  ;; Год-2000;
-	(time-hour nil)	  ;; Час
-	(time-minute nil) ;; Минута
-	(time-second nil) ;; Секунда
-
-	(name-wid 10)
-	(opis-wid 40)
-	(dim-wid  24)
-	(num-rec-analog 0)
-	(num-rec-diskret 0)
-	)
-    (setf
-     id (recode-string (read-trd-file in head-id-wid))
-     version  (car(read-trd-file in head-version-wid))
-     bufer (read-trd-file in head-date-wid)
-     date-day (first bufer)
-     date-month (second bufer)
-     date-year (+ 2000 (third bufer))
-     bufer (read-trd-file in head-time-wid)
-     time-hour (first bufer)
-     time-minute (second bufer)
-     time-second (third bufer)
-     )
-    (format t "~S ~A ~S-~S-~S_~S:~S:~S "
-	    id version date-year date-month date-day time-hour time-minute time-second)))
-
-;;    (dotimes (i head-wid) (push (read-byte in) head))
-;;    (setf head (reverse head))
-;;    (setf num-rec-analog (+ (nth 26 head) (* 256 (nth 27 head)))
-;;	  num-rec-diskret (+ (nth 28 head) (* 256 (nth 29 head))))
-;;    (dotimes (i (* num-rec-analog (+ name-wid opis-wid dim-wid))) (push (read-byte in) analog))
-;;    (setf analog (reverse analog))
-;;    (dotimes (i (* num-rec-diskret (+ name-wid opis-wid))) (push (read-byte in) discret))
-;;    (setf  discret (reverse discret))
-;;    (close in)
 
 (defun read-trd-file-short(in)
   "Выполняет чтение short из потока in"
@@ -155,11 +58,109 @@
     (dotimes (i 8 rez)
       (setf (ldb (byte 8  (* 8 i)) rez) (read-byte in)))))
 
-;;;;(open-trd-file #p"/home/namatv/MyDoc/git/clisp/recoder/230415_191202.trd")
+(defun read-trd-file-float(in)
+  "Выполняет чтение long-long из потока in"
+  (let ((rez 0))
+    (dotimes (i 4 (ie3fp:decode-ieee-float rez))
+      (setf (ldb (byte 8  (* 8 i)) rez) (read-byte in)))))
+
+(defun read-trd-file-double(in)
+  "Выполняет чтение long-long из потока in"
+  (let ((rez 0))
+    (dotimes (i 8 (ie3fp:decode-ieee-double rez))
+      (setf (ldb (byte 8  (* 8 i)) rez) (read-byte in)))))
+
+(defun read-trd-file-quad (in)
+  "Выполняет чтение long-long из потока in"
+  (let ((rez 0))
+    (dotimes (i 16 (ie3fp:decode-ieee-quad rez))
+      (setf (ldb (byte 8  (* 8 i)) rez) (read-byte in)))))
+
+(defun read-trend-hdr (in &optional (byte-number 30))
+  (let ((bufer nil)   ;; Буфер для чтения данных
+	(analog nil)  ;; Аналоговые сигналы
+	(discret nil) ;; Дискретные сигналы
+	(head-wid 30)
+	(head-id-wid 5)		     ;; Строка идентификации
+	(head-version-wid 1)	     ;; Версия данных тренда
+	(head-date-wid 3)	     ;; День Месяц Год-2000
+	(head-time-wid 3)	     ;; Час Минута Секунда
+	(id nil)		     ;; Строка идентификации
+	(version nil)		     ;; Версия данных трендера
+	(date-day nil)		     ;; День
+	(date-month nil)	     ;; Месяц 
+	(date-year nil)		     ;; Год-2000;
+	(time-hour nil)		     ;; Час
+	(time-minute nil)	     ;; Минута
+	(time-second nil)	     ;; Секунда
+	(reserv nil)		     ;; Резерв
+	(total-records nil)	     ;; Общее число записей
+	(delta-time nil)	     ;; Интервал времени
+	(analog-number nil)	     ;; Количество аналоговых сигналов
+        (discret-number nil)	     ;; Количество дискретных сигналов
+	(analog-id-wid 10)	     ;; char[10]
+	(analog-description-wid 40)  ;; char[40]
+	(analog-units-wid  8)	     ;; char[8]
+	(analog-LowLimit-wid 8)	     ;; double
+	(analog-HighLimit-wid 8)     ;; double
+  	(discret-id-wid 10)	     ;; char[10]
+	(discret-description-wid 40) ;; char[40]
+	(analog-id nil)
+	(analog-description nil)
+	(analog-units  nil)
+	(analog-LowLimit nil)
+	(analog-HighLimit nil)
+  	(discret-id nil)
+	(discret-description nil)
+	(analog-descriptor-list nil)
+	(discret-descriptor-list nil)
+	)
+    (setf
+     id (recode-string (read-trd-file in head-id-wid))
+     version  (car(read-trd-file in head-version-wid))
+     bufer (read-trd-file in head-date-wid)
+     date-day (first bufer)
+     date-month (second bufer)
+     date-year (+ 2000 (third bufer))
+     bufer (read-trd-file in head-time-wid)
+     time-hour (first bufer)
+     time-minute (second bufer)
+     time-second (third bufer)
+     reserv (read-trd-file-short in)
+     total-records (read-trd-file-long in)
+     delta-time (read-trd-file-double in)
+     analog-number (read-trd-file-short in)
+     discret-number (read-trd-file-short in)
+     )
+    (dotimes (i analog-number (setf analog-descriptor-list (nreverse analog-descriptor-list)))
+      (setf analog-id (recode-string (read-trd-file in analog-id-wid))
+	    analog-description (recode-string (read-trd-file in analog-description-wid))
+	    analog-units  (recode-string (read-trd-file in analog-units-wid))
+	    analog-LowLimit (read-trd-file-double in)
+	    analog-HighLimit (read-trd-file-double in))
+      (push (list analog-id
+		  analog-description
+		  analog-units
+		  analog-LowLimit
+		  analog-HighLimit) analog-descriptor-list))
+    (dotimes (i discret-number (setf discret-descriptor-list (nreverse discret-descriptor-list)))
+      (setf discret-id (recode-string (read-trd-file in discret-id-wid))
+	    discret-description (recode-string (read-trd-file in discret-description-wid)))
+      (push (list discret-id
+		  discret-description
+		  ) discret-descriptor-list))
+    (format t "~S ~A ~S-~S-~S_~S:~S:~S "
+	    id version date-year date-month date-day time-hour time-minute time-second)
+    (format t "~%Reserv         = ~A~%Total-records  = ~A~%Delta-time     = ~A~%Analog-number  = ~A~%Discret-number = ~A" reserv total-records delta-time analog-number discret-number)
+    analog-descriptor-list))
+
+;;;;(defparameter in (open-trd-file #p"/home/namatv/MyDoc/git/clisp/recoder/230415_191202.trd"))
+;;;;(defparameter in (open-trd-file #p"/home/namatv/1.txt"))
 
 (progn
-  (defparameter in (open-trd-file #p"/home/namatv/1.txt"))
-  (defparameter b_1 (read-trd-file-short     in))
-  (defparameter b_2 (read-trd-file-int       in))
-  (defparameter b_3 (read-trd-file-long-long in))
+  (defparameter in
+    (open-trd-file #p"/home/namatv/MyDoc/git/clisp/recoder/230415_191202.trd"))
+  (defparameter analog-signal-list(read-trend-hdr in))
   (close in))
+
+
