1c292dcf9070fa530d020d785fd100021507cbd7
Nick Matvyeyev <mnasoft@gmail.com>
2015-05-10 18:23:26 +0300

    Выполнен небольшой рефакторинг с целью исключения повторяющегося кода

--- recoder.lisp
+++ recoder.lisp
@@ -15,7 +15,7 @@
     (setf str-rez (concatenate 'string str-rez ch))))
 
 (defun open-trd-file(path)
-  "Выполняет открытие файла тренда."
+  "Выполняет открытие файла тренда"
   (open path :element-type 'unsigned-byte))
 
 (defun read-trd-file(in byte-number)
@@ -32,49 +32,34 @@
     (dotimes (i len rez)
       (setf (ldb (byte 8  (* 8 i)) rez) (nth i list-of-int)))))
 
-
-
-(defun read-trd-file-short(in)
+(defun read-trd-file-short(in &optional (len 2))
   "Выполняет чтение short из потока in"
-  (let ((rez 0))
-    (dotimes (i 2 rez)
-      (setf (ldb (byte 8  (* 8 i)) rez) (read-byte in)))))
+  (list-to-int(read-trd-file in len)))
 
-(defun read-trd-file-int(in)
+(defun read-trd-file-int(in &optional (len 4))
   "Выполняет чтение int из потока in"
-  (let ((rez 0))
-    (dotimes (i 4 rez)
-      (setf (ldb (byte 8  (* 8 i)) rez) (read-byte in)))))
+  (list-to-int(read-trd-file in len)))
 
-(defun read-trd-file-long(in)
+(defun read-trd-file-long(in &optional (len 4))
   "Выполняет чтение long из потока in"
-  (let ((rez 0))
-    (dotimes (i 4 rez)
-      (setf (ldb (byte 8  (* 8 i)) rez) (read-byte in)))))
+  (list-to-int(read-trd-file in len)))
 
-(defun read-trd-file-long-long(in)
+(defun read-trd-file-long-long(in &optional (len 8)))
   "Выполняет чтение long-long из потока in"
-  (let ((rez 0))
-    (dotimes (i 8 rez)
-      (setf (ldb (byte 8  (* 8 i)) rez) (read-byte in)))))
+  (list-to-int(read-trd-file in len)))
 
-(defun read-trd-file-float(in)
-  "Выполняет чтение long-long из потока in"
-  (let ((rez 0))
-    (dotimes (i 4 (ie3fp:decode-ieee-float rez))
-      (setf (ldb (byte 8  (* 8 i)) rez) (read-byte in)))))
+(defun read-trd-file-float(in &optional (len 4))
+  "Выполняет чтение float из потока in"
+    (ie3fp:decode-ieee-float(list-to-int(read-trd-file in len))))
 
-(defun read-trd-file-double(in)
-  "Выполняет чтение long-long из потока in"
-  (let ((rez 0))
-    (dotimes (i 8 (ie3fp:decode-ieee-double rez))
-      (setf (ldb (byte 8  (* 8 i)) rez) (read-byte in)))))
+(defun read-trd-file-double(in &optional (len 8))
+  "Выполняет чтение doudle из потока in"
+  (ie3fp:decode-ieee-double (list-to-int(read-trd-file in len))))
 
-(defun read-trd-file-quad (in)
-  "Выполняет чтение long-long из потока in"
-  (let ((rez 0))
-    (dotimes (i 16 (ie3fp:decode-ieee-quad rez))
-      (setf (ldb (byte 8  (* 8 i)) rez) (read-byte in)))))
+(defun read-trd-file-quad (in &optional (len 16))
+  "Выполняет чтение quad из потока in"
+  (ie3fp:decode-ieee-quad (list-to-int(read-trd-file in len))))
+
 
 (defun read-trend-hdr (in &optional (byte-number 30))
   (let ((bufer nil)   ;; Буфер для чтения данных
