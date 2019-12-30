ac23ed46ebc4d3fd91b0ed61b38d288e23832e0c
Nick Matvyeyev <mnasoft@gmail.com>
2019-12-30 11:46:48 +0200

    Пн дек 30 09:46:48 UTC 2019

---
+++ docs/manifest.lisp
@@ -0,0 +1,6 @@
+(:docstring-markup-format :scriba
+ :systems (:recoder)
+ :documents ((:title "Recoder"
+		     :authors ("Nick Matvyeyev")
+		     :output-format (:type :multi-html :template :minima)
+		     :sources ("manual.scr"))))
