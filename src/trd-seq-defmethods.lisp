82c665d0e92de1d1ba8573d2a74d3aea50d9eba9
Nick Matvyeyev <mnasoft@gmail.com>
2020-11-09 07:30:06 +0200

    Пн 09 ноя 2020 05:30:06 UTC

--- src/trd-seq-defmethods.lisp
+++ src/trd-seq-defmethods.lisp
@@ -2,19 +2,47 @@
 
 (in-package #:recoder)
 
-(export '(<trd-seq> <trd-seq>-a-sig <trd-seq>-d-sig update <trd-seq>-signal-strings))
+(export '(<trd-seq> <trd-seq>-a-sig <trd-seq>-d-sig update <trd-seq>-s-sig))
 
 (defclass <trd-seq> (<trd> sequence)
-  ((signal-strings :accessor <trd-seq>-signal-strings :initform nil :initarg :signal-strings)
-   (a-sig :accessor <trd-seq>-a-sig :initform nil :documentation "Список аналоговых сигналов.")
-   (d-sig :accessor <trd-seq>-d-sig :initform nil :documentation "Список дискретных сигналов.")))
+  ((s-sig :reader <trd-seq>-s-sig :initform nil :initarg :s-sig
+          :documentation "Список с именами сигналов.")
+   (a-sig :accessor <trd-seq>-a-sig :initform nil
+          :documentation "Список аналоговых сигналов.")
+   (d-sig :accessor <trd-seq>-d-sig :initform nil
+          :documentation "Список дискретных сигналов.")
+   (h-tbl :accessor <trd-seq>-h-tbl :initform (make-hash-table :test #'equal)
+          :documentation "Хешированная таблица: 
+@begin(list)
+ @item(ключ - имя сигнала;)
+ @item(значение - номер сигнала в записи  Список дискретных сигналов.)
+@end(list) "))
+  (:documentation "@b(Описание:) класс @b(<trd-seq>) реализует 
+протоколы доступа к записям тренда через протоколы
+доступа к элементам последовательности.
+"))
+
+(defmethod (setf <trd-seq>-s-sig) (new-value (trd-seq <trd-seq>))
+  (unless (trd-file-descr trd-seq) (trd-open trd-seq))
+  (with-slots (s-sig) trd-seq
+    (setf s-sig new-value)
+    (update trd-seq)))
 
 (defmethod update ((trd-seq <trd-seq>))
   "@b(Описание:) метод @b(update) 
 "
-  (let ((sig (trd-separate-signals trd-seq (<trd-seq>-signal-strings trd-seq))))
+  (unless (trd-file-descr trd-seq) (trd-open trd-seq))
+  (let ((sig (trd-separate-signals trd-seq (<trd-seq>-s-sig trd-seq))))
     (setf (<trd-seq>-a-sig trd-seq) (first  sig))
     (setf (<trd-seq>-d-sig trd-seq) (second sig))
+    (with-slots (s-sig) trd-seq
+      (setf s-sig
+            (append (mapcar #'recoder/a-signal:a-signal-id (<trd-seq>-a-sig trd-seq))
+                    (mapcar #'recoder/d-signal:d-signal-id (<trd-seq>-d-sig trd-seq)))))
+    (clrhash (<trd-seq>-h-tbl trd-seq))
+    (loop :for s :in (<trd-seq>-s-sig trd-seq)
+	  :for i :from 0 :below (length (<trd-seq>-s-sig trd-seq))
+	  :do  (setf (gethash s (<trd-seq>-h-tbl trd-seq)) i ))
     trd-seq))
 
 (defmethod sequence:length ((trd-seq <trd-seq>))
@@ -25,8 +53,10 @@
   (unless (trd-file-descr trd-seq) (trd-open trd-seq))
   (let ((a-sig (<trd-seq>-a-sig trd-seq))
         (d-sig (<trd-seq>-d-sig trd-seq)))
-    (append (when a-sig (trd-analog-by-rec-number  trd-seq index a-sig))
-            (when d-sig (trd-discret-by-rec-number trd-seq index d-sig)))))
+    (coerce
+     (append (when a-sig (trd-analog-by-rec-number  trd-seq index a-sig))
+             (when d-sig (trd-discret-by-rec-number trd-seq index d-sig)))
+     'vector)))
 
 (defmethod trd-open :after ((trd-seq <trd-seq>))
   "@b(Описание:) метод @b(trd-open :after)
@@ -34,4 +64,19 @@
   (update trd-seq))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-   
+
+(defmethod sig     (key data (trd-seq <trd-seq>))
+  (svref data (gethash key (<trd-seq>-h-tbl trd-seq))))
+
+(defmethod sig-on  (key data (trd-seq <trd-seq>))
+  (= 1 (sig key data trd-seq)))
+
+(defmethod sig-off (key data (trd-seq <trd-seq>))
+  (= 0 (sig key data trd-seq)))
+
+(sig "GQ010" (elt *trd* 10000) *trd*)
+
+(sig-off "FK310" (elt *trd* 10000) *trd*)
+
+(<trd-seq>-d-sig *trd*)
+
