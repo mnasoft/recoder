e27b24f293ad5d7d44ed7f557051cbf01fbac274
Nick Matvyeyev <mnasoft@gmail.com>
2021-06-10 15:58:41 +0300

    10 июн 2021 г. 15:58:41

--- src/d-signal/d-signal-defmethods.lisp
+++ src/d-signal/d-signal-defmethods.lisp
@@ -1,25 +1,32 @@
 ;;;; d-signal-defmethods.lisp
 
-(in-package #:recoder/d-signal)
+(defpackage #:recoder/d-signal
+  (:use #:cl)
+  ;; #:mnas-string #:recoder/binary
+  (:export <d-signal>
+	   d-signal-description
+	   d-signal-id
+           d-signal-num))
 
-(export '(<d-signal> d-signal-num d-signal-id d-signal-description))
-        
+;;;;(declaim (optimize (space 0) (compilation-speed 0)  (speed 0) (safety 3) (debug 3)))
+;;;; (declaim (optimize (compilation-speed 0) (debug 3) (safety 0) (space 0) (speed 0)))
+
+(in-package #:recoder/d-signal)
+      
 (defclass <d-signal> ()
   ((d-signal-num         :accessor d-signal-num         :initarg :d-signal-num         :initform nil :documentation "Номер сигнала в списке сигналов. Первый сигнал имеет номер 0")
    (d-signal-id          :accessor d-signal-id          :initarg :d-signal-id          :initform nil :documentation "Обозначение сигнала, char[10]")
    (d-signal-description :accessor d-signal-description :initarg :d-signal-description :initform nil :documentation "Описание сигнала, char[40]"))
   (:documentation "Дескриптор (описание) дискретного сигнала.
 
-Запись дескриптора аналогового сигнала во внутреннем представлении
+ Запись дескриптора аналогового сигнала во внутреннем представлении
 файло-тренда имеет следующую структуру:
+
 @begin(table)
 @begin(row) @cell(Поле)               @cell(Длина поля)@cell(Примечание) @end(row)
 @begin(row) @cell(discret-id)         @cell(10)        @cell(Обозначение дискретного сигнала) @end(row)
 @begin(row) @cell(discret-description)@cell(40)        @cell(Описание дискретного сигнала) @end(row)
-@end(table)
-"))
-
+@end(table)"))
 
 (defmethod print-object ((x <d-signal>) stream)
   (format stream "~S ~S ~S" (d-signal-num x) (d-signal-id x) (d-signal-description x)))
-
