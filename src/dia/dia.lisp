8f214b90ce6dd1c69d27ad0971beddd797762ca9
Nick Matvyeyev <mnasoft@gmail.com>
2021-09-28 11:11:27 +0300

    28 сен 2021 г.  8:11:27

--- src/dia/dia.lisp
+++ src/dia/dia.lisp
@@ -5,25 +5,43 @@
   (:nicknames "R/DIA")
   (:export get-open-ternds
 	   get-open-ternd
-	   change-directory-default))
+	   change-directory-default)
+  (:export *trd*))
 
 (in-package #:recoder/dia)
 
+(defparameter *trd* nil
+  "@b(Описание:) переменная @b(*trd*) содержит объект класса @b(recoder/trd:<trd>).
+")
+
 (defun get-open-ternd ()
-  "@b(Описание:) функция @b(get-open-ternd)
+  "@b(Описание:) функция @b(get-open-ternd) возвращает объект класса
+  @b(recoder/trd:<trd>).
 
  @b(Пример использования:)
 @begin[lang=lisp](code)
  (get-open-ternd)
 @end(code)"
-  (mnas-file-dialog:get-open-file
-   :filetypes '(("Файлы трендов" "*.trd"))
-   :title "Выберите файл тренда"))
+  (setf *trd*
+        (r/trd:trd-open
+         (make-instance 'recoder/trd:<trd> :file-name
+                        (mnas-file-dialog:get-open-file
+                         :filetypes '(("Файлы трендов" "*.trd"))
+                         :title "Выберите файл тренда")))))
 
 (defun get-open-ternds ()
+  "@b(Описание:) функция @b(get-open-ternds) возвращает список имен,
+ отобранных в диалоге файлов.
+
+ @b(Пример использования:) @begin[lang=lisp](code)
+ (get-open-ternds)
+@end(code)
+
+"
   (mnas-file-dialog:get-open-file
    :filetypes '(("Файлы трендов" "*.trd"))
    :title "Выберите файлы трендов" :multiple t))
 
 (defun change-directory-default ()
+  
   (mnas-file-dialog:change-directory-default))
