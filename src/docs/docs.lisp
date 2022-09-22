45e21cbc6408dd146712549cc4ffb3d9f28e05cd
Nick Matvyeyev <mnasoft@gmail.com>
2022-09-22 08:54:46 +0300

    22 сен 2022 г.  5:54:46

--- src/docs/docs.lisp
+++ src/docs/docs.lisp
@@ -50,26 +50,22 @@
 (defun make-all (&aux
                    (of (if (find (uiop:hostname)
                                  mnas-package:*intranet-hosts*
-                                 :test #'string=)
+                                 :test #'string= :key #'first)
                            '(:type :multi-html :template :gamma)
                            '(:type :multi-html :template :minima))))
-  "@b(Описание:) функция @b(make-all) служит для создания документации.
-
- Пакет документации формируется в каталоге
-~/public_html/Common-Lisp-Programs/recoder.
-"
-  (mnas-package:make-html-path :recoder)
-  (make-document)
-  (make-graphs)
-  (mnas-package:make-mainfest-lisp
-   '(:recoder :recoder/docs)
-   "Recoder"
-   '("Mykola Matvyeyev")
-   (mnas-package:find-sources "recoder")
-   :output-format of)
-  (codex:document :recoder)
-  (make-graphs)
-  (mnas-package:copy-doc->public-html "recoder")
-  (mnas-package:rsync-doc "recoder"))
+  (let* ((sys-symbol :recoder)
+         (sys-string (string-downcase (format nil "~a" sys-symbol))))
+    (mnas-package:make-html-path sys-symbol)
+    (make-document)
+    (mnas-package:make-mainfest-lisp `(,sys-symbol)
+                                     (string-capitalize sys-string)
+                                     '("Mykola Matvyeyev")
+                                     (mnas-package:find-sources sys-symbol)
+                                     :output-format of)
+    (codex:document sys-symbol)
+    (make-graphs)
+    (mnas-package:copy-doc->public-html sys-string)
+    (mnas-package:rsync-doc sys-string)
+    :make-all-finish))
 
 ;;;; (make-all)
