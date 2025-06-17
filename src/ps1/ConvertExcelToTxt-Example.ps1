594079a0e8ae14a891c4bff8678ddaa134d3649e
Mykola Matvyeyev <mnasoft@gmail.com>
2025-06-17 22:11:24 +0300

    Вт 17 июн 2025 19:11:24 UTC

--- src/ps1/ConvertExcelToTxt-Example.ps1
+++ src/ps1/ConvertExcelToTxt-Example.ps1
@@ -1,1 +1,17 @@
 powershell -ExecutionPolicy Bypass -File ConvertExcelToTxt.ps1 -InputFile "C:\вход.xlsx" -OutputFile "C:\выход.txt" -SkipRows 15
+
+
+
+$start = $ws.Range("A15")
+$end = $start.End(-4167) # -4167 — xlUp (Ctrl + ↑)
+Write-Host "Новая ячейка: $($end.Address)"
+Write-Host "Значение: $($end.Value())"
+
+1 — xlToLeft (Ctrl + ←)
+
+2 — xlToRight (Ctrl + →)
+
+3 — xlDown (Ctrl + ↓)
+
+-4167 — xlUp (Ctrl + ↑)
+
