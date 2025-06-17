709b94fda3e0affba22f8fc8eb415ab7b743f7af
Nick Matvyeyev <mnasoft@gmail.com>
2025-06-17 07:50:58 +0300

    17 июн 2025 г.  4:50:58

--- src/trd/test.lisp
+++ src/trd/test.lisp
@@ -39,3 +39,45 @@
                      (r/slist:a-signals  *trd1* '("Gv" "V2" "P02" "P03"))
                      (r/slist:d-signals  *trd1* '("NJ010" "NJ020" "GAS" "OIL"))
                      ))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defparameter *fn*
+  (probe-file
+   "D:/home/_namatv/PRG/msys64/home/namatv/quicklisp/local-projects/clisp/recoder/trd/1_Custom_sec_14April2025_10226_PM.txt"))
+
+(with-open-file (in *fn* :external-format :utf-16le )
+  (let* ((names-256 (ppcre:split #\Tab (read-line in)))
+        (cols (position-if
+               #'(lambda (el)
+                   (or (string= el "") (string= el "")))
+               names-256)))
+    (loop :for name :in (cdddr names-256)
+          :for n :from 0 :below (- cols 3)
+          :collect
+          (let ((a-signal (make-instance 'r/a-sig:<a-signal>))
+                (name-3 (foo name) )
+                )
+            (setf  (r/a-sig:<a-signal>-num a-signal) n)
+            (setf  (r/a-sig:<a-signal>-id  a-signal) (first name-3))
+            (setf  (r/a-sig:<a-signal>-description  a-signal) (second name-3))
+            (setf  (r/a-sig:<a-signal>-units  a-signal) (third name-3))
+            a-signal
+            )
+          
+          )))
+
+
+(defun foo (string)
+  (multiple-value-bind (whole m1)
+      (ppcre:scan-to-strings
+       "^([^:]*):?([^,]*),?(.*)$" string)
+    (loop :for i :across m1
+          :collect (string-trim " " i))))
+
+(foo "EB040: Вибрация корпуса ГТД (КС), vv2")
+
+
+  (make-instance 'r/a-sig:<a-signal>)
+
+
+
