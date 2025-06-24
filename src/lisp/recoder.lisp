35a90fa863ad4918113e40d1c14c2a090127d599
Nick Matvyeyev <mnasoft@gmail.com>
2025-06-24 15:26:49 +0300

    24 июн 2025 г. 12:26:49

--- src/lisp/recoder.lisp
+++ src/lisp/recoder.lisp
@@ -45,25 +45,29 @@
            (format t "~3A ~A~%" n i)
            (recode i)))
 
+(defparameter *123*
+    (mapcar
+     #'pathname
+     (mnas-file-dialog:get-open-file :filetypes '(("XLS" "*.xls")) :multiple t)))
+
+(recode  (probe-file (first *123*)))
+
 (defun recode (path)
   "@b(Описание:) функция @b(path) выполняет перекодирование тренда из
 формата xls или txt в формат трендера trd.
 
  При перекодировании функция основывается на расширении файла.
 "
-  (let ((ps1-script
-          (probe-file
-           (first
-            (directory
-             (concatenate 'string
-                          (namestring (mnas-path:posix-arg0-path))
-                          "*.ps1"))))))
-    (format t "~A~%"ps1-script)
+  (let* ((ps1-script-fn 
+           (first (directory (concatenate 'string (namestring (mnas-path:posix-arg0-path)) "*.ps1"))))
+         (ps1-script
+           (when ps1-script-fn
+             (probe-file ps1-script-fn))))
+    (format t "~A~%" ps1-script)
     (cond
       (ps1-script
        (setf r/trd:*convert-excel-to-txt-ps1* ps1-script))
       ((probe-file r/trd:*convert-excel-to-txt-ps1*))
-
       (t (error "~A" r/trd:*convert-excel-to-txt-ps1*)))
     (let ((trd (make-instance 'r/c:<trd>)))
       (r/g:read-obj trd path)
